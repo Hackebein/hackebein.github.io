3eb4b5e37100283f63ffdd3a756826c24c18fcbc
Lars Olzem <hackebein@gmail.com>
2024-11-09 12:22:53 +0000

    fix: post create and tasks commands

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -9,7 +9,7 @@ echo "$GPG_PRIVATE_ID:6:" | gpg --import-ownertrust
 go install github.com/emad-elsaid/xlog/cmd/xlog@latest
 
 # Install dnscontrol types
-docker run --rm -it -v \"$(pwd)/dns:/dns\" ghcr.io/stackexchange/dnscontrol write-types
+docker run --rm -it -v "$(pwd)/dns:/dns" ghcr.io/stackexchange/dnscontrol write-types
 
 # Install live-server
 npm install -g live-server
